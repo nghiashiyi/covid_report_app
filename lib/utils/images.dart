ebc8d06a65cb2125751295c823003dc6ac855add
nghia <nghia.tsy@gmail.com>
2020-08-01 12:07:20 +0700

    feat: add resources

---
+++ lib/utils/images.dart
@@ -0,0 +1,12 @@
+class AppImages {
+  static const _folderPath = '/assets';
+  static const String icSearch = _folderPath + 'ic_search.png';
+  static const String img1 = _folderPath + 'img_1.png';
+  static const String img2 = _folderPath + 'img_2.png';
+  static const String img3 = _folderPath + 'img_3.png';
+  static const String img4 = _folderPath + 'img_4.png';
+  static const String img5 = _folderPath + 'img_5.png';
+  static const String imgGlobal = _folderPath + 'img_global.png';
+  static const String imgUs = _folderPath + 'img_us.png';
+  static const String imgVn = _folderPath + 'img_vietnam.png';
+}
